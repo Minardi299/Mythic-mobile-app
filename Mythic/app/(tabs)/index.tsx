9efbaee4b679759b9a4237600ad91fca111f4cae
Minh <phu.ngo@outlook.com>
2025-09-08 14:58:53 -0400

    add image from expo

--- Mythic/app/(tabs)/index.tsx
+++ Mythic/app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { Text, View,  StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageViewer';
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
@@ -26,5 +28,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width:320,
+    height: 440,
+    borderRadius: 18,
   },
 });
